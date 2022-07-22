d54b4f30275bd5cb399b2959c586e919794ee2e7
Pavel Chernook <Maklauren97@gmail.com>
2022-07-22 16:07:58 +0300

    Initial commit

---
+++ SeeFood-CoreML/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeeFood-CoreML
+//
+//  Created by Павел Черноок on 22.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
